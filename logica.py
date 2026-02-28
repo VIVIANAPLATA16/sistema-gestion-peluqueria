713bb730da787261285efef883853e9b613bf13f
VIVIANAPLATA16 <vivisplata@hotmail.com>
2026-02-28 12:42:35 -0500

    Update logica.py

--- logica.py
+++ logica.py
@@ -1,4 +1,5 @@
 from database import conectar
+
 def registrar_cliente(nombre, telefono):
     conn = conectar()
     cursor = conn.cursor()
@@ -13,3 +14,25 @@ def listar_clientes():
     res = cursor.fetchall()
     conn.close()
     return res
+
+def buscar_cliente(nombre):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM clientes WHERE nombre LIKE ?", ('%' + nombre + '%',))
+    res = cursor.fetchall()
+    conn.close()
+    return res
+
+def editar_cliente(id_cliente, nuevo_nombre, nuevo_tel):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE clientes SET nombre = ?, telefono = ? WHERE id = ?", (nuevo_nombre, nuevo_tel, id_cliente))
+    conn.commit()
+    conn.close()
+
+def eliminar_cliente(id_cliente):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM clientes WHERE id = ?", (id_cliente,))
+    conn.commit()
+    conn.close()
